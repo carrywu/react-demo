e035900091bb7345d350d22aca0227f8a41a1585
carry <773275204@qq.com>
2020-06-23 17:25:47 +0800

    feat:lesson4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
- class App extends React.Component{
-   render(){
-     return(
-       <div>
-         hello world
-       </div> 
-     )
-   }
- }
+import React, { Component, Fragment } from 'react';
+import "./style.css"
+import { CSSTransition } from 'react-transition-group'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      show:true
+    }
+    this.handleToggle=this.handleToggle.bind(this);  
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <CSSTransition in={this.state.show}
+        timeout={1000}
+        classNames='fade'
+        unmountOnExit
+        >
+        <div >hello</div>
+        </CSSTransition>
+        <button onClick={this.handleToggle}>toggle</button>
+   
+      </Fragment>
+
+    )
+  }
+  handleToggle(){
+    this.setState({
+      show:this.state.show?false:true
+    })
+  }
+}
 
 export default App;
